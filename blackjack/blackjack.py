be02b24e7ed038aa5401605de430010a91c49a06
oswal118 <oswal118@umn.edu>
2022-04-28 18:51:23 -0500

    editing blackjack game

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,9 +1,30 @@
 # BlackJack or 21 game
+import random
+import os
+import time
+
+
 # THE PLANNING PHASE
-    # Dealer cards
-    # Player cards
-    # Deal the cards
-    # Display the cards
+# Declaring variable for dealer cards
+dealer_cards = []
+# Declaring variable for player cards
+player_cards = []
+
+# Deal the cards
+# Dealer Cards
+while len(dealer_cards) != 2:
+    dealer_cards.append(random.randint(1, 11))
+    if len(dealer_cards) == 2:
+        print("Dealer has X & ", dealer_cards[1])
+
+
+# Player Cards
+while len(player_cards) != 2:
+    player_cards.append(random.randint(1, 11))
+    if len(player_cards) == 2:
+        print("You have ", player_cards)
+
+
     # Sum of the Dealer cards
     # Sum of the Player cards
     # Compare the sums of the cards between Dealer & Player
