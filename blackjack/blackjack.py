f914b93797c32c4150a751370467c692d957dc02
oswal118 <oswal118@umn.edu>
2022-04-28 17:35:17 -0500

    initial commit

---
+++ blackjack/blackjack.py
@@ -0,0 +1,14 @@
+# BlackJack or 21 game
+# THE PLANNING PHASE
+    # Dealer cards
+    # Player cards
+    # Deal the cards
+    # Display the cards
+    # Sum of the Dealer cards
+    # Sum of the Player cards
+    # Compare the sums of the cards between Dealer & Player
+    # If P card sum is greater than 21 = BUST
+    # If P card sum is less than 21 = Option Hit or Stay
+    # If P option Stay compare sum of Dealer vs Player
+    # If P sum < 21 && > D sum then P wins
+    # If P sum < D sum then P loses
