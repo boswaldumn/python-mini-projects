7c632a193d24570719adf7e28e6bdabed4413af7
oswal118 <oswal118@umn.edu>
2022-04-28 21:10:30 -0500

    adding fleetwood mac bot

---
+++ discord-bot/fleetwood-mac-bot.py
@@ -0,0 +1,12 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("hello"):
+        await message.channel.send("Hello, I am a big fan of fleetwood mac")
+
+client.run('OTY5Mzk3NTk4MTk0NjMwNjY3.Ymsz3Q.ti8F0GoBPegd17sUrQAvfYaTmk4')
