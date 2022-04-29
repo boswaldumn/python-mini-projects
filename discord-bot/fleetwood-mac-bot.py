eda610daa0f13bcd53ef087e4ea213ce6bd53c56
oswal118 <oswal118@umn.edu>
2022-04-28 21:32:57 -0500

    editing README

--- discord-bot/fleetwood-mac-bot.py
+++ discord-bot/fleetwood-mac-bot.py
@@ -9,4 +9,4 @@ async def on_message(message):
     if message.content.startswith("hello"):
         await message.channel.send("Hello, I am a big fan of fleetwood mac")
 
-client.run('OTY5Mzk3NTk4MTk0NjMwNjY3.Ymsz3Q.Z0UROsss_HzNoMbaKZpgLYxUtMQ')
+client.run('YOUR_TOKEN_HERE')
